0d567cb9038a0d71a3a4b8ea7ab4ade0716ed174
goUp9 <steven999420@gmail.com>
2025-03-25 03:32:15 -0400

    add interfaces

--- index.ts
+++ index.ts
@@ -1,1 +1,40 @@
 
+import Client, {
+    CommitmentLevel,
+    SubscribeRequest,
+    SubscribeUpdate,
+    SubscribeUpdateTransaction,
+} from "@triton-one/yellowstone-grpc";
+import {ClientDuplexStream} from "@grpc/grpc-js";
+import {MessageAddressTableLookup, MessageHeader, PublicKey} from '@solana/web3.js';
+
+//Interfaces
+interface CompiledInstruction {
+    programIdIndex: number;
+    accounts: Uint8Array;
+    data: Uint8Array;
+}
+
+interface Message {
+    header: MessageHeader | undefined;
+    accountKeys: Uint8Array[];
+    recentBlockhash: Uint8Array;
+    instructions: CompiledInstruction[];
+    versioned: boolean;
+    addressTableLookups: MessageAddressTableLookup[];
+}
+
+interface MessageHeader {
+    numRequiredSignatures: number;
+    numReadonlySignedAccounts: number;
+    numReadonlyUnsignedAccounts: number;
+}
+
+interface MessageAddressTableLookup {
+    accountkey: Uint8Array;
+    writableIndexes: Uint8Array;
+    readonlyIndexes: Uint8Array;
+}
+
+
+
